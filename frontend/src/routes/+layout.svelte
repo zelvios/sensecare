ed4128f4c2d019a998dd666034215cd9edafde01
jacob <me@jacob-j.com>
2026-09-11 00:03:51 +0200

    feat(frontend): viewportheight layout and default border colour

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-  import type { Pathname } from '$app/types';
-  import { resolve } from '$app/paths';
-  import { page } from '$app/state';
-  import { locales, localizeHref } from '$lib/paraglide/runtime';
+  import '@fontsource-variable/noto-sans';
   import './layout.css';
   import favicon from '$lib/assets/favicon.svg';
 
@@ -10,10 +7,7 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
 
-<div style="display:none">
-  {#each locales as locale (locale)}
-    <a href={resolve(localizeHref(page.url.pathname, { locale }) as Pathname)}>{locale}</a>
-  {/each}
+<div class="flex h-dvh flex-col">
+  {@render children()}
 </div>
